4543f4ca05f93bdfe67fbe5351c1d8338561d322
Ozodbekyum <hello7ozod@gamil.com>
2022-08-20 11:16:05 +0500

    for05

--- bin/for_loop05.dart
+++ bin/for_loop05.dart
@@ -1,12 +1,22 @@
 int func(List<int> numbers) {
 /*
-   Given the list of numbers, return sum the odd numbers in the list
+   Given the list of numbers, 
+   return sum the odd numbers in the list
 Args:
   numbers(List): value
 Return: 
   Int: answer
 */
-  return 0;
+  int o = 0;
+  for (int i = 0; i < numbers.length; i = i + 1) {
+    if (numbers[i] % 2 == 1) {
+      o += numbers[i];
+    }
+  }
+
+  return o;
 }
 
-void main() {}
+void main() {
+  print(func([5, 3, 2, 8, 5]));
+}
